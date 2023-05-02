f1c624702c72173e6699072bf60e812e0c6c9af0
Laurent PEREIRA <laurent.pereira1@gmail.com>
2023-05-02 10:16:32 -0400

    Step2

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Grid, CssBaseline, Container, TextField, Typography, Alert } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import axios from 'axios';
@@ -7,15 +7,15 @@ const Homepage = () => {
     const [ number, setNumber ] = useState(0);
     const [ roman, setRoman ] = useState('');
     const [ error, setError ] = useState('');
+    const [ session, setSession ] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        axios.post(process.env.REACT_APP_API_URL, { number }).then((res) => {
-            setRoman(res.data.result);
+        axios.post(process.env.REACT_APP_API_URL, { number, session }).then((_) => {
             setError('');
         }).catch((err) => {
-            setRoman('');
             setError(err.response.data.details[0].message);
+            setRoman('');
         });
     };
 
@@ -23,6 +23,25 @@ const Homepage = () => {
         const { value } = e.target;
         setNumber(parseInt(value, 10));
     };
+
+    useEffect(() => {
+        if(!session) {
+            const eventSource = new EventSource(`${process.env.REACT_APP_API_URL}`);
+            eventSource.onmessage = (event) => {
+                const { id, result } = JSON.parse(event.data);
+                setSession(id);
+                setRoman(result);
+                setError('');
+            };
+            eventSource.onerror = (error) => {
+                setError('Unable to connect to server, please try again later');
+                setSession(null);
+            };
+            eventSource.onclose = () => {
+                eventSource.close();
+            };
+        }
+    }, [session]);
 
     return (
         <Grid container component="main">
@@ -33,6 +52,7 @@ const Homepage = () => {
                     <Typography variant="h1" component="h1" gutterBottom align='center'> 
                         Jolimoi
                     </Typography>
+                    <Typography variant="body1" gutterBottom align='center'>{session  ? `Connected to server on session ${session}` : 'Disconnected to server'}</Typography>
                     <TextField
                         label="Nombre"
                         variant="outlined"
@@ -45,6 +65,7 @@ const Homepage = () => {
                     />
                     <LoadingButton
                         loading={false}
+                        disabled={session === ''}
                         onClick={handleSubmit}
                         variant="contained"
                         sx={{ margin: '10px'}}
@@ -52,7 +73,7 @@ const Homepage = () => {
                 </Container>
                 <Container>
                     {error && <Alert severity="error">{error}</Alert>}
-                    {roman && <Alert severity="info">{`Le chiffre romain correspondant à ${number} est ${roman}`}</Alert>}
+                    {roman !== '' && <Alert severity="info">{`Le chiffre romain correspondant est ${roman}`}</Alert>}
                 </Container>
             </Grid>
             <Grid item md={3} />
